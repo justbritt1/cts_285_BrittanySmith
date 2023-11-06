15cf66ca3e0c15da61f732ffdfecda3a8024ce0f
justbritt1 <smithb8349@student.faytechcc.edu>
2023-11-06 12:35:07 -0500

    Made app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+
+
+
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from dataman_calculator import add, subtract, multiply, divide
-from auth import db, User, init_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use SQLite as the database
 app.secret_key = 'your_secret_key'
+db = SQLAlchemy()
 db.init_app(app)
 
-# ... (other routes)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+
+# Initialize memory bank
+memory = 0
+
+# ... (other calculator functions)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,7 +41,7 @@ def login():
 
     return render_template('login.html', message='')
 
-# ... (other routes)
+# ... (other Flask routes)
 
 if __name__ == '__main__':
     with app.app_context():
